c6e01ce93a91f0894ed5211c981a61b297608369
Narmada Padala <narmadapadala@gmail.com>
2026-01-13 15:06:15 -0800

    Add staging models for members, claims, and plans

--- sql/sql/02_staging_models.sql
+++ sql/sql/02_staging_models.sql
@@ -1,1 +1,29 @@
+-- Staging members table
+-- Cleaned and standardized enrollment data
+SELECT
+  member_id,
+  plan_type,
+  enrollment_date,
+  termination_date,
+  age,
+  gender
+FROM members;
+
+
+-- Staging claims table
+-- Cleaned claims cost data
+SELECT
+  claim_id,
+  member_id,
+  service_date,
+  claim_amount,
+  claim_type
+FROM claims;
+
+-- Staging plans reference table
+SELECT
+  plan_type,
+  plan_name,
+  metal_level
+FROM plans;
 
